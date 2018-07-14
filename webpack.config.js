bdfb24bcb4a93370c01eb694221b356690a83e42
Matt Nardoni <mattnardoni@MattNardoni.local>
2018-07-14 14:24:06 -0700

    Update project name in config files

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'Social Media Clone',
+      title: 'Capstone Project Plan',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
