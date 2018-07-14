c64600236e3deb52d4929eb211a20af1caa21bd1
Matt Nardoni <mattnardoni@MattNardoni.local>
2018-07-14 16:30:28 -0700

    Turned off auto indent to fix error, added homepage file content

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,3 +1,23 @@
 import React from 'react';
 
-function HomePage() 
+function HomePage() {
+  return (
+    <div id='login'>
+<div>
+      <style jsx>{`
+  p {
+    text-indent: 30pt;
+  }
+`}</style>
+</div>
+<div id='logo'>
+<h2>SPORTSBASE</h2>
+</div>
+<div id='button'>
+<button>Continue</button>
+</div>
+    </div>
+)
+}
+
+export default HomePage;
